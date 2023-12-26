1379c1dac478dabbe96765f4e677ccde713cf4f6
Víctor Barrios <sygmaz19@gmail.com>
2023-12-27 00:11:32 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Víctor Barrios Sánchez on 27/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
